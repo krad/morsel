e8874aee724e954d8443293c997e8854492f834a
krad <iam@krad.io>
2017-12-07 22:03:47 -0700

    Dumb bug where I was sending the wrong segment.  Need to cleanup and refactor

--- Sources/morsel/FragmentedMP4Writer.swift
+++ Sources/morsel/FragmentedMP4Writer.swift
@@ -81,7 +81,7 @@ extension FragmentedMP4Writer: StreamSegmenterDelegate {
     func createNewSegment(with segmentID: Int, and sequenceNumber: Int) {
         if let segment = self.currentSegment {
             self.playerListWriter.write(segment: segment)
-            self.delegate?.wroteFile(at: self.segmenter!.currentSegmentURL)
+            self.delegate?.wroteFile(at: segment.file)
         }
         
         self.currentSegment = try? FragmentedMP4Segment(self.segmenter!.currentSegmentURL,
