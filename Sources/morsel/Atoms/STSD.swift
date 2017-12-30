d75ddaaa6b9a233894ee3698ec003a93460d2254
krad <iam@krad.io>
2017-12-30 15:01:05 -0600

    Removing my debug helper

--- Sources/morsel/Atoms/STSD.swift
+++ Sources/morsel/Atoms/STSD.swift
@@ -1,6 +1,3 @@
-// FIXME
-import Foundation
-
 struct STSD: BinarySizedEncodable {
     
     let type: Atom = .stsd
@@ -15,24 +12,6 @@ struct STSD: BinarySizedEncodable {
     static func from(_ config: VideoSettings) -> STSD {
         var stsd = STSD()
         stsd.avc1 = [AVC1.from(config)]
-        
-        // FIXME: Remove.  This is here to test integration with cisco's bullshit h264 thing
-        if let avc1 = stsd.avc1 {
-            let avc1Bytes = try? BinaryEncoder.encode(avc1)
-            
-            let documentsPath = NSSearchPathForDirectoriesInDomains(.documentDirectory,
-                                                                    .userDomainMask, true)[0]
-            let uuid = UUID().uuidString
-            let url  = URL(fileURLWithPath: documentsPath).appendingPathComponent("\(uuid).h264_params")
-            let data = Data(avc1Bytes!)
-            do {
-                print("Writing data to", url)
-                try data.write(to: url)
-            } catch let err {
-                print("Could not write data", err)
-            }
-        }
-        
         stsd.mp4a = nil
         return stsd
     }
