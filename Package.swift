442b956485bb7163c1d6c403f9951cee2dd466f6
krad <iam@krad.io>
2018-02-19 14:10:26 -0600

    Explicity adding the .git to the Package dependency

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
             targets: ["morsel"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/krad/grip" , from: "1.0.3"),
+        .package(url: "https://github.com/krad/grip.git" , from: "1.0.3"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
